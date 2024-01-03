03c4c6cbc543762d667ebd5a1f8221e48f5b315f
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2024-01-03 14:29:23 +0530

    Sql sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node_complete',
-    password:'Asif@766735'
-})
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node_complete',
+//     password:'Asif@766735'
+// })
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node_complete','root','Asif@766735',{
+    dialect :'mysql',
+    host : 'localhost'
+});
+
+module.exports=sequelize;
